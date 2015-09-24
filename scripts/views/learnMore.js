f167736da714b030a2be1690a183a018f22da4e5
onehunnid <climbrick1989@gmail.com>
2015-09-23 22:01:45 -0400

    added data and initLearnMore function

--- scripts/views/learnMore.js
+++ scripts/views/learnMore.js
@@ -1,7 +1,14 @@
 var LearnMore = (function() {
 
+	var learnMoreData = {
+		title: 'Freedom of Speech',
+		desc: 'We believe in the power of transparency. Our goal is to empower users across the globe through our anonymous tool and encourage self expression.',
+	},
+	template = $('#learnMoreTemplate').html(),
+	compiled = _.template( template );
+
 	function initLearnMore() {
-		$('#main-content').html('<h1>Learn More</h1>');
+		$('#main-content').html(compiled(learnMoreData));
 	}
 	return {
 		init: initLearnMore
