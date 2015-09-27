f39c8ae7237c41834324cab15617f01872cfcc6f
Chris Limbrick <climbrick@Chriss-MacBook-Air.local>
2015-09-27 17:53:16 -0400

    search page

--- scripts/app.js
+++ scripts/app.js
@@ -10,8 +10,14 @@
 		LearnMore.init();
 	};
 
+	Views.search = function search() {
+		console.log(Search);
+		Search.init();
+	};
+
 	Routes.register('home', Views.home );
 	Routes.register('learnMore', Views.learnMore );
+	Routes.register('search', Views.search );
 
 	Routes.init('home');
 	
